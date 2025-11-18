23daf6220edaa0c28278b072dfd01cef64c369fb
Muyang <977155494@qq.com>
2025-11-18 12:46:22 -0500

    重新上传

---
+++ leetcode/590. N-ary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    private List<Integer> ans;
+
+    private void traverse(Node root){
+        if (root == null) {
+			return;
+		}
+		for(Node n:root.children) {
+			traverse(n);
+		}
+        ans.add(root.val);
+    }
+
+    public List<Integer> postorder(Node root) {
+        ans = new ArrayList<Integer>();
+        traverse(root);
+        return ans;
+    }
+}
