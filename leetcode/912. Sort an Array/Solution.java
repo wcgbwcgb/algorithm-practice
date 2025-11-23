b7e56864a3994e1421b7998ab0f2527c334859ef
Muyang <977155494@qq.com>
2025-11-23 16:39:39 -0500

    完成Leetcode912

---
+++ leetcode/912. Sort an Array/Solution.java
@@ -0,0 +1,53 @@
+class Solution {
+private void sink(int[] nums, int index, int size) {
+		int left = index*2 + 1;
+		int right = index*2 + 2;
+		int min = index;
+		
+		while(left < size || right < size) {
+			left = index*2 + 1;
+			right = index*2 + 2;
+			min = index;
+			if(left < size && nums[left] > nums[min]) {
+				min = left;
+			}
+			if(right < size && nums[right] > nums[min]) {
+				min = right;
+			}
+			if(min == index) {
+				break;
+			}
+			swap(nums, index, min);
+			index = min;
+		}
+	}
+	
+	private void swim(int[] nums, int index) {
+		if(index < 1 || nums[index] <= nums[(index-1)/2]) {
+			return;
+		}
+		swap(nums, index, (index-1)/2);
+		swim(nums, (index-1)/2);
+	}
+	
+	private void swap(int[] nums, int idx1, int idx2) {
+		int temp;
+		temp = nums[idx2];
+		nums[idx2] = nums[idx1];
+		nums[idx1] = temp;
+	}
+    
+    public int[] sortArray(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+			swim(nums, i);
+		}
+		
+		int size = nums.length;
+		while(size>0) {
+			swap(nums, 0, size-1);
+			size--;
+			sink(nums, 0, size);
+		}
+        return nums;
+    }
+}
